e7ba938242c70d7126cba99a6695ca834214b3a0
JM <jessmorrison90@gmail.com>
2018-09-07 08:57:37 -0500

    Commit - 1
    Adding Project Into Repository.
    Hello World - essentially

---
+++ LibraryCardCatalog/LibraryCardCatalog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCardCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lol github");
+            Console.ReadLine();
+        }
+    }
+}
